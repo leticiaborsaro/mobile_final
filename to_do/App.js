4588eecc8b1643d1c1c04b71a6faa18ab31e3405
Letícia Borsaro <leleborsaro@gmail.com>
2026-04-06 21:17:25 -0300

    changes

--- to_do/App.js
+++ to_do/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Lista de Tarefas</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: 'black',
+    fontSize: 20, 
+    textAlign:'center'
+  }
 });
