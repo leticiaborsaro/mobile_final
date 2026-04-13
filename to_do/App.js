246e2cd882a84c641f226b6cebf2347586f6dba6
Letícia Borsaro <leleborsaro@gmail.com>
2026-04-13 14:59:14 -0300

    changes 1

--- to_do/App.js
+++ to_do/App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput, Switch, Button, Flatlist } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { Slider } from '@react-native-community/slider';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function TelaInicial({ navigation }) {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}>Lista de Tarefas</Text>
+        <StatusBar style="auto" />
+      </View>
+  );
+}
+
+function TelaProjeto() {
+  return (
+    pass
+  );
+}
+
+function TelaTarefa() {
+  return (
+    pass
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Lista de Tarefas</Text>
-      <StatusBar style="auto" />
-    </View>
+  //Container para navegação
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={TelaInicial}/>
+      <Stack.Screen name="Projeto" component={TelaProjeto}/>
+      <Stack.Screen name="Tarefa" component={TelaTarefa}/>
+
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
